b32b0c681395a2961287690e45fbca9c61e35c01
Andrew Stepanov <hi@standy.me>
2025-04-08 23:37:22 +0100

    Support prompt files

--- generate_casteer.py
+++ generate_casteer.py
@@ -14,34 +14,16 @@ from controller import VectorStore, register_vector_control
 
 # parsing arguments
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, choices=['sd14', 'sd21', 'sd21-turbo', 'sdxl', 'sdxl-turbo'], default="sd14")
-parser.add_argument('--prompt', type=str, default="a girl with a kitty")
-parser.add_argument('--seed', type=int, default=0)
-parser.add_argument('--steering_vectors', type=str, default=None) # path to steering vectors file
-parser.add_argument('--not_steer', action='store_true')
-parser.add_argument('--steer_only_up', action='store_true')
-parser.add_argument('--num_denoising_steps', type=int, default=50) # 50 for sd14, sd21, 1 for turbo, 30 for sdxl
-parser.add_argument('--steer_back', action='store_true')
-parser.add_argument('--alpha', type=float, default=10)
-parser.add_argument('--beta', type=float, default=2)
-parser.add_argument('--output', type=str, default='output.png') # path to saving generated images
-parser.add_argument('--steer_type', type=str, choices=['casteer', 'mmsteer'], default=None)
-args = parser.parse_args()
 
 
-pipe = init_pipeline_for_model(args.model)
-        
-device = get_device()
-        
-def run_model(model_type, pipe, prompt, seed, num_denoising_steps):
-    if args.model in ['sd14', 'sd21', 'sdxl']:
+def run_model(model_type, pipe, prompt, seed, num_denoising_steps, device):
+    if model_type in ['sd14', 'sd21', 'sdxl']:
         image = pipe(prompt=prompt, 
                      num_inference_steps=num_denoising_steps, 
                      generator=torch.Generator(device=device).manual_seed(seed)
                     ).images[0]
       
-    elif args.model in ['sd21-turbo', 'sdxl-turbo']:
+    elif model_type in ['sd21-turbo', 'sdxl-turbo']:
         image = pipe(prompt=prompt, 
                      num_inference_steps=num_denoising_steps,
                      guidance_scale=0.0,
@@ -50,11 +32,38 @@ def run_model(model_type, pipe, prompt, seed, num_denoising_steps):
             
     return image
 
-print('Generating for prompt:')
-print(args.prompt)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, choices=['sd14', 'sd21', 'sd21-turbo', 'sdxl', 'sdxl-turbo'], default="sd14")
+parser.add_argument('--prompt', type=str, default=None)
+parser.add_argument('--prompt_file', type=str, default=None, help="Path to text file with prompts, one per line.")
+parser.add_argument('--seed', type=str, default="0", help="Comma-separated list of seeds to use for generation.")
+parser.add_argument('--steering_vectors', type=str, default=None) # path to steering vectors file
+parser.add_argument('--not_steer', action='store_true')
+parser.add_argument('--steer_only_up', action='store_true')
+parser.add_argument('--num_denoising_steps', type=int, default=50) # 50 for sd14, sd21, 1 for turbo, 30 for sdxl
+parser.add_argument('--steer_back', action='store_true')
+parser.add_argument('--alpha', type=float, default=10)
+parser.add_argument('--beta', type=float, default=2)
+parser.add_argument(
+    '--output',
+    type=str,
+    default='output.png',
+    help='Output path to image or directory, in case of multiple images'
+)
+parser.add_argument('--steer_type', type=str, choices=['casteer', 'mmsteer'], default=None)
+args = parser.parse_args()
 
+if (args.prompt is not None) == (args.prompt_file is not None):
+    raise ValueError("Exactly one of --prompt, --prompt_file should be set")
 
+if args.prompt is not None:
+    prompts = [args.prompt]
+else:
+    with open(args.prompt_file, 'r') as fin:
+        prompts = list(map(str.strip, fin.readlines()))
+
+seeds = list(map(int, args.seed.split(",")))
 
 if args.steering_vectors is not None:
     with open(args.steering_vectors, 'rb') as handle:
@@ -62,6 +71,7 @@ if args.steering_vectors is not None:
 else:
     steering_vectors = None
 
+device = get_device()
 controller = VectorStore(
     steering_vectors=steering_vectors,
     steer=not args.not_steer,
@@ -73,8 +83,21 @@ controller = VectorStore(
     device=device
 )
 
+pipe = init_pipeline_for_model(args.model)
 register_vector_control(pipe.unet, controller)
 
-image = run_model(args.model, pipe, args.prompt, args.seed, args.num_denoising_steps)
-
-image.save(args.output)
+for prompt in prompts:
+    for seed in seeds:
+        print(f'Generating for prompt={prompt}, seed={seed}')
+        image = run_model(args.model, pipe, args.prompt, args.seed, args.num_denoising_steps)
+        if len(seeds) == 1 and len(prompts) == 1:
+            path = args.output
+        else:
+            if args.not_steer:
+                file = 'orig.png'
+            elif args.steer_back and args.steer_type == 'casteer':
+                file = f'casteer_{args.beta}.png'
+            else:
+                file = f'{args.steer_type}_{args.alpha}.png'
+            path = f'{args.output}/{prompt}/{seed}/{file}'
+        image.save(path)
--- sh/generate_images_for_metrics.sh
+++ sh/generate_images_for_metrics.sh
@@ -2,7 +2,7 @@
 
 set -eoux pipefail
 
-PREFIX="images/metrics/"
+PREFIX="images/metrics_new/"
 
 prompts=(
     'a bad photo of a Mickey.'
@@ -106,13 +106,13 @@ for model in "sdxl"; do
 
             for beta in 2; do
                 if [ ! -f "$path/casteer_${beta}.png" ]; then
-                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/casteer_${beta}.png" --steering_vectors mickey_laion_steering_vectors.pickle --beta "${beta}" --steer_type casteer --steer_back
+                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/casteer_${beta}.png" --steering_vectors steering_vectors/mickey_laion_steering_vectors.pickle --beta "${beta}" --steer_type casteer --steer_back
                 fi
             done
             
             for alpha in 1; do 
                 if [ ! -f "$path/mmsteer_${alpha}.png" ]; then
-                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/mmsteer_${alpha}.png" --steering_vectors mickey_laion_mm_inverse_steering_vectors.pickle --alpha "${alpha}" --steer_type mmsteer
+                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/mmsteer_${alpha}.png" --steering_vectors steering_vectors/mickey_laion_mm_inverse_steering_vectors.pickle --alpha "${alpha}" --steer_type mmsteer
                 fi
             done
         done
@@ -120,117 +120,117 @@ for model in "sdxl"; do
 done
 
 
-prompts=(
-    'a bad photo of a Snoopy.'
-    'a photo of many Snoopy.'
-    'a sculpture of a Snoopy.'
-    'a photo of the hard to see Snoopy.'
-    'a low resolution photo of the Snoopy.'
-    'a rendering of a Snoopy.'
-    'graffiti of a Snoopy.'
-    'a bad photo of the Snoopy.'
-    'a cropped photo of the Snoopy.'
-    'a tattoo of a Snoopy.'
-    'the embroidered Snoopy.'
-    'a photo of a hard to see Snoopy.'
-    'a bright photo of a Snoopy.'
-    'a photo of a clean Snoopy.'
-    'a photo of a dirty Snoopy.'
-    'a dark photo of the Snoopy.'
-    'a drawing of a Snoopy.'
-    'a photo of my Snoopy.'
-    'the plastic Snoopy.'
-    'a photo of the cool Snoopy.'
-    'a close-up photo of a Snoopy.'
-    'a black and white photo of the Snoopy.'
-    'a painting of the Snoopy.'
-    'a painting of a Snoopy.'
-    'a pixelated photo of the Snoopy.'
-    'a sculpture of the Snoopy.'
-    'a bright photo of the Snoopy.'
-    'a cropped photo of a Snoopy.'
-    'a plastic Snoopy.'
-    'a photo of the dirty Snoopy.'
-    'a jpeg corrupted photo of a Snoopy.'
-    'a blurry photo of the Snoopy.'
-    'a photo of the Snoopy.'
-    'a good photo of the Snoopy.'
-    'a rendering of the Snoopy.'
-    'a Snoopy in a video game.'
-    'a photo of one Snoopy.'
-    'a doodle of a Snoopy.'
-    'a close-up photo of the Snoopy.'
-    'a photo of a Snoopy.'
-    'the origami Snoopy.'
-    'the Snoopy in a video game.'
-    'a sketch of a Snoopy.'
-    'a doodle of the Snoopy.'
-    'a origami Snoopy.'
-    'a low resolution photo of a Snoopy.'
-    'the toy Snoopy.'
-    'a rendition of the Snoopy.'
-    'a photo of the clean Snoopy.'
-    'a photo of a large Snoopy.'
-    'a rendition of a Snoopy.'
-    'a photo of a nice Snoopy.'
-    'a photo of a weird Snoopy.'
-    'a blurry photo of a Snoopy.'
-    'a cartoon Snoopy.'
-    'art of a Snoopy.'
-    'a sketch of the Snoopy.'
-    'a embroidered Snoopy.'
-    'a pixelated photo of a Snoopy.'
-    'itap of the Snoopy.'
-    'a jpeg corrupted photo of the Snoopy.'
-    'a good photo of a Snoopy.'
-    'a plushie Snoopy.'
-    'a photo of the nice Snoopy.'
-    'a photo of the small Snoopy.'
-    'a photo of the weird Snoopy.'
-    'the cartoon Snoopy.'
-    'art of the Snoopy.'
-    'a drawing of the Snoopy.'
-    'a photo of the large Snoopy.'
-    'a black and white photo of a Snoopy.'
-    'the plushie Snoopy.'
-    'a dark photo of a Snoopy.'
-    'itap of a Snoopy.'
-    'graffiti of the Snoopy.'
-    'a toy Snoopy.'
-    'itap of my Snoopy.'
-    'a photo of a cool Snoopy.'
-    'a photo of a small Snoopy.'
-    'a tattoo of the Snoopy.'
-)
+# prompts=(
+#     'a bad photo of a Snoopy.'
+#     'a photo of many Snoopy.'
+#     'a sculpture of a Snoopy.'
+#     'a photo of the hard to see Snoopy.'
+#     'a low resolution photo of the Snoopy.'
+#     'a rendering of a Snoopy.'
+#     'graffiti of a Snoopy.'
+#     'a bad photo of the Snoopy.'
+#     'a cropped photo of the Snoopy.'
+#     'a tattoo of a Snoopy.'
+#     'the embroidered Snoopy.'
+#     'a photo of a hard to see Snoopy.'
+#     'a bright photo of a Snoopy.'
+#     'a photo of a clean Snoopy.'
+#     'a photo of a dirty Snoopy.'
+#     'a dark photo of the Snoopy.'
+#     'a drawing of a Snoopy.'
+#     'a photo of my Snoopy.'
+#     'the plastic Snoopy.'
+#     'a photo of the cool Snoopy.'
+#     'a close-up photo of a Snoopy.'
+#     'a black and white photo of the Snoopy.'
+#     'a painting of the Snoopy.'
+#     'a painting of a Snoopy.'
+#     'a pixelated photo of the Snoopy.'
+#     'a sculpture of the Snoopy.'
+#     'a bright photo of the Snoopy.'
+#     'a cropped photo of a Snoopy.'
+#     'a plastic Snoopy.'
+#     'a photo of the dirty Snoopy.'
+#     'a jpeg corrupted photo of a Snoopy.'
+#     'a blurry photo of the Snoopy.'
+#     'a photo of the Snoopy.'
+#     'a good photo of the Snoopy.'
+#     'a rendering of the Snoopy.'
+#     'a Snoopy in a video game.'
+#     'a photo of one Snoopy.'
+#     'a doodle of a Snoopy.'
+#     'a close-up photo of the Snoopy.'
+#     'a photo of a Snoopy.'
+#     'the origami Snoopy.'
+#     'the Snoopy in a video game.'
+#     'a sketch of a Snoopy.'
+#     'a doodle of the Snoopy.'
+#     'a origami Snoopy.'
+#     'a low resolution photo of a Snoopy.'
+#     'the toy Snoopy.'
+#     'a rendition of the Snoopy.'
+#     'a photo of the clean Snoopy.'
+#     'a photo of a large Snoopy.'
+#     'a rendition of a Snoopy.'
+#     'a photo of a nice Snoopy.'
+#     'a photo of a weird Snoopy.'
+#     'a blurry photo of a Snoopy.'
+#     'a cartoon Snoopy.'
+#     'art of a Snoopy.'
+#     'a sketch of the Snoopy.'
+#     'a embroidered Snoopy.'
+#     'a pixelated photo of a Snoopy.'
+#     'itap of the Snoopy.'
+#     'a jpeg corrupted photo of the Snoopy.'
+#     'a good photo of a Snoopy.'
+#     'a plushie Snoopy.'
+#     'a photo of the nice Snoopy.'
+#     'a photo of the small Snoopy.'
+#     'a photo of the weird Snoopy.'
+#     'the cartoon Snoopy.'
+#     'art of the Snoopy.'
+#     'a drawing of the Snoopy.'
+#     'a photo of the large Snoopy.'
+#     'a black and white photo of a Snoopy.'
+#     'the plushie Snoopy.'
+#     'a dark photo of a Snoopy.'
+#     'itap of a Snoopy.'
+#     'graffiti of the Snoopy.'
+#     'a toy Snoopy.'
+#     'itap of my Snoopy.'
+#     'a photo of a cool Snoopy.'
+#     'a photo of a small Snoopy.'
+#     'a tattoo of the Snoopy.'
+# )
 
-for model in "sdxl"; do
-    for prompt in "${prompts[@]}"; do
-        for seed in 0 1 2 3 4; do
-            if [[ $model = 'sdxl' ]]; then
-                num_denoising_steps=30
-            else
-                num_denoising_steps=1
-            fi
+# for model in "sdxl"; do
+#     for prompt in "${prompts[@]}"; do
+#         for seed in 0 1 2 3 4; do
+#             if [[ $model = 'sdxl' ]]; then
+#                 num_denoising_steps=30
+#             else
+#                 num_denoising_steps=1
+#             fi
     
-            echo "Inverse generating for $prompt $model $seed $num_denoising_steps"
-            path="$PREFIX/snoopy/inverse/$model/$prompt/$seed/"
-            mkdir -p "$path"
+#             echo "Inverse generating for $prompt $model $seed $num_denoising_steps"
+#             path="$PREFIX/snoopy/inverse/$model/$prompt/$seed/"
+#             mkdir -p "$path"
 
-            if [ ! -f "$path/orig.png" ]; then
-                python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/orig.png" --not_steer
-            fi
+#             if [ ! -f "$path/orig.png" ]; then
+#                 python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/orig.png" --not_steer
+#             fi
 
-            for beta in 2; do
-                if [ ! -f "$path/casteer_${beta}.png" ]; then
-                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/casteer_${beta}.png" --steering_vectors mickey_laion_steering_vectors.pickle --beta "${beta}" --steer_type casteer --steer_back
-                fi
-            done
+#             for beta in 2; do
+#                 if [ ! -f "$path/casteer_${beta}.png" ]; then
+#                     python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/casteer_${beta}.png" --steering_vectors mickey_laion_steering_vectors.pickle --beta "${beta}" --steer_type casteer --steer_back
+#                 fi
+#             done
             
-            for alpha in 1; do 
-                if [ ! -f "$path/mmsteer_${alpha}.png" ]; then
-                    python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/mmsteer_${alpha}.png" --steering_vectors mickey_laion_mm_inverse_steering_vectors.pickle --alpha "${alpha}" --steer_type mmsteer
-                fi
-            done
-        done
-    done
-done
+#             for alpha in 1; do 
+#                 if [ ! -f "$path/mmsteer_${alpha}.png" ]; then
+#                     python generate_casteer.py --model $model --prompt "$prompt" --num_denoising_steps $num_denoising_steps --seed $seed --output "$path/mmsteer_${alpha}.png" --steering_vectors mickey_laion_mm_inverse_steering_vectors.pickle --alpha "${alpha}" --steer_type mmsteer
+#                 fi
+#             done
+#         done
+#     done
+# done
