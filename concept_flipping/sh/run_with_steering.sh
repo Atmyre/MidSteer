f864b64c42541ea9ad47da49512e6baefb09715b
Andrew Stepanov <hi@standy.me>
2025-05-14 21:16:22 +0100

    Update script

--- concept_flipping/sh/run_with_steering.sh
+++ concept_flipping/sh/run_with_steering.sh
@@ -8,14 +8,16 @@ layer_type=self_attn
 source_concept=horses
 target_concept=motorcycles
 max_new_tokens=150
-# mu_neutral_path=./concept_flipping/cov/meta-llama_Llama-2-7b-chat-hf_self_attn_means_2000.pt
-# cov_neutral_path=./concept_flipping/cov/meta-llama_Llama-2-7b-chat-hf_self_attn_covariances_2000.pt
+mu_neutral_path=./ckpt/llama2-7b-chat_alpaca_all_self_attn_all/pos_means_1000.pickle
+cov_neutral_path=./ckpt/llama2-7b-chat_alpaca_all_self_attn_all/pos_covariances_1000.pickle
 
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. python concept_flipping/run_with_steering.py \
     --model_name $model_name \
     --layer_type $layer_type \
     --source_concept $source_concept \
     --target_concept $target_concept \
-    --steer_type casteer \
+    --steer_type mean_matching \
     --strength 2.0 \
-    --max_new_tokens $max_new_tokens
+    --max_new_tokens $max_new_tokens \
+    --mu_neutral $mu_neutral_path \
+    --cov_neutral $cov_neutral_path
