63f27c2c73655cd2e4c220927cd91199018e4374
Tatiana Gaintseva <Gaintseva.t@gmail.com>
2025-07-11 22:42:32 +0000

    New batch of experiments

--- llm_exp/sh/slurm_base_experiment.sh
+++ llm_exp/sh/slurm_base_experiment.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #SBATCH --partition=h100-camera-train
-#SBATCH --gpus=1
+#SBATCH --gpus=4
 #SBATCH --time=36:00:00
 #SBATCH --output=./llm_exp/logs/slurm-%x-%j.out
 #SBATCH --error=./llm_exp/logs/slurm-%x-%j.err
@@ -10,9 +10,10 @@ set -eoux pipefail
 
 # Check if required arguments are provided
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 <layer_type> <num_covariances> <token_aggregation_mode> <max_new_tokens> [--mm_normalize_centers] [--intermediate_clipping] [--renormalize_after_steering]"
+    echo "Usage: $0 <layer_type> <num_covariances> <token_aggregation_mode> <max_new_tokens> [strengths] [--mm_normalize_centers] [--intermediate_clipping] [--renormalize_after_steering]"
     echo "Example: $0 self_attn 20000 all 100" 
-    echo "Example with optional flags: $0 self_attn 20000 all 100 --mm_normalize_centers --intermediate_clipping"
+    echo "Example with strengths: $0 self_attn 20000 all 100 '1.0 2.0 3.0'"
+    echo "Example with optional flags: $0 self_attn 20000 all 100 '1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0' --mm_normalize_centers --intermediate_clipping"
     exit 1
 fi
 
@@ -21,12 +22,25 @@ layer_type=$1
 num_covariances=$2
 token_aggregation_mode=$3
 max_new_tokens=$4
+
+# Set default strengths
+default_strengths="1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0"
+
+# Check if 5th argument is provided and doesn't start with --
+if [ $# -gt 4 ] && [[ "$5" != --* ]]; then
+    strengths="$5"
+    start_idx=6
+else
+    strengths="$default_strengths"
+    start_idx=5
+fi
+
 mm_normalize_centers=""
 intermediate_clipping=""
 renormalize_after_steering=""
 
 # Check for optional arguments
-for arg in "${@:5}"; do
+for arg in "${@:$start_idx}"; do
     case $arg in
         "--mm_normalize_centers")
             mm_normalize_centers="--mm_normalize_centers"
@@ -62,38 +76,38 @@ covariances_dir=$base_dir/covariances
 topics="horses motorcycles cats dogs"
 steering_vectors_dir=$base_dir/steering_vectors
 
-# if [ $num_covariances -eq 0 ]; then
-#     $run_cmd $python ./llm_estimate_covariances.py \
-#         --model_name $model_name \
-#         --layer_type $layer_type \
-#         --token_aggregation_mode $token_aggregation_mode \
-#         --num_samples 10 \
-#         --max_new_tokens $max_new_tokens \
-#         --output_dir $covariances_dir &
+if [ $num_covariances -eq 0 ]; then
+    $run_cmd $python ./llm_estimate_covariances.py \
+        --model_name $model_name \
+        --layer_type $layer_type \
+        --token_aggregation_mode $token_aggregation_mode \
+        --num_samples 10 \
+        --max_new_tokens $max_new_tokens \
+        --output_dir $covariances_dir &
 
-#     additional_steering_params="$additional_steering_params --identity_cov --zero_mu_neutral"
-# else
-#     $run_cmd $python ./llm_estimate_covariances.py \
-#         --model_name $model_name \
-#         --layer_type $layer_type \
-#         --token_aggregation_mode $token_aggregation_mode \
-#         --num_samples $num_covariances \
-#         --max_new_tokens $max_new_tokens \
-#         --output_dir $covariances_dir &
+    additional_steering_params="$additional_steering_params --identity_cov --zero_mu_neutral"
+else
+    $run_cmd $python ./llm_estimate_covariances.py \
+        --model_name $model_name \
+        --layer_type $layer_type \
+        --token_aggregation_mode $token_aggregation_mode \
+        --num_samples $num_covariances \
+        --max_new_tokens $max_new_tokens \
+        --output_dir $covariances_dir &
 
-# fi
+fi
 
 
-# $run_cmd $python ./llm_generate_steering_vectors.py \
-#     --model_name $model_name \
-#     --layer_type $layer_type \
-#     --topics $topics \
-#     --token_aggregation_mode last \
-#     --max_new_tokens 1 \
-#     --num_samples 1000 \
-#     --output_dir $steering_vectors_dir &
+$run_cmd $python ./llm_generate_steering_vectors.py \
+    --model_name $model_name \
+    --layer_type $layer_type \
+    --topics $topics \
+    --token_aggregation_mode last \
+    --max_new_tokens 1 \
+    --num_samples 1000 \
+    --output_dir $steering_vectors_dir &
 
-# wait
+wait
 
 
 
@@ -113,86 +127,88 @@ declare -A concept_pairs=(
     ["dogs"]="cats"
 )
 
-# for source_concept in "${!concept_pairs[@]}"; do
-#     target_concept="${concept_pairs[$source_concept]}"
+for source_concept in "${!concept_pairs[@]}"; do
+    target_concept="${concept_pairs[$source_concept]}"
     
-#     results_subdir="$results_dir/${source_concept}_to_${target_concept}"
-#     mkdir -p "$results_subdir"
+    results_subdir="$results_dir/${source_concept}_to_${target_concept}"
+    mkdir -p "$results_subdir"
 
-#     # Define evaluation parameters as arrays
-#     declare -a eval_params=(
-#         "--dataset_type template --samples_per_question $concept_samples_per_question --max_new_tokens $concept_max_new_tokens --output_dir \"$results_subdir/eval\""
-#         "--dataset_type alpaca --num_samples $consistency_num_samples --samples_per_question $consistency_samples_per_question --max_new_tokens $consistency_max_new_tokens --output_dir \"$results_subdir/alpaca\""
-#         "--dataset_type mmlu --num_samples $consistency_num_samples --samples_per_question $consistency_samples_per_question --max_new_tokens $consistency_max_new_tokens --output_dir \"$results_subdir/mmlu\""
-#     )
+    # Define evaluation parameters as arrays
+    declare -a eval_params=(
+        "--dataset_type template --samples_per_question $concept_samples_per_question --max_new_tokens $concept_max_new_tokens --output_dir \"$results_subdir/eval\""
+        "--dataset_type alpaca --num_samples $consistency_num_samples --samples_per_question $consistency_samples_per_question --max_new_tokens $consistency_max_new_tokens --output_dir \"$results_subdir/alpaca\""
+        "--dataset_type mmlu --num_samples $consistency_num_samples --samples_per_question $consistency_samples_per_question --max_new_tokens $consistency_max_new_tokens --output_dir \"$results_subdir/mmlu\""
+    )
 
-#     for params in "${eval_params[@]}"; do
-#         $run_cmd $python ./llm_run_with_steering.py \
-#             --model_name $model_name \
-#             --layer_type $layer_type \
-#             --source_concept $source_concept \
-#             --strength 0.0 \
-#             $additional_steering_params \
-#             $params &
+    for params in "${eval_params[@]}"; do
+        $run_cmd $python ./llm_run_with_steering.py \
+            --model_name $model_name \
+            --layer_type $layer_type \
+            --source_concept $source_concept \
+            --strength 0.0 \
+            $additional_steering_params \
+            $params &
 
-#         $run_cmd $python ./llm_run_with_steering.py \
-#             --model_name $model_name \
-#             --layer_type $layer_type \
-#             --source_concept $source_concept \
-#             --source_concept_path $steering_vectors_dir/$source_concept.pt \
-#             --target_concept_path $steering_vectors_dir/$target_concept.pt \
-#             --steer_type casteer \
-#             --strength 2.0 \
-#             --mu_neutral $covariances_dir/means.pt \
-#             --cov_neutral $covariances_dir/covariances.pt \
-#             $additional_steering_params \
-#             $params &
+        for strength in $strengths; do
+            $run_cmd $python ./llm_run_with_steering.py \
+                --model_name $model_name \
+                --layer_type $layer_type \
+                --source_concept $source_concept \
+                --source_concept_path $steering_vectors_dir/$source_concept.pt \
+                --target_concept_path $steering_vectors_dir/$target_concept.pt \
+                --steer_type casteer \
+                --strength $strength \
+                --mu_neutral $covariances_dir/means.pt \
+                --cov_neutral $covariances_dir/covariances.pt \
+                $additional_steering_params \
+                $params &
+        done
 
-#         $run_cmd $python ./llm_run_with_steering.py \
-#             --model_name $model_name \
-#             --layer_type $layer_type \
-#             --source_concept $source_concept \
-#             --source_concept_path $steering_vectors_dir/$source_concept.pt \
-#             --target_concept_path $steering_vectors_dir/$target_concept.pt \
-#             --steer_type leace \
-#             --strength 2.0 \
-#             --mu_neutral $covariances_dir/means.pt \
-#             --cov_neutral $covariances_dir/covariances.pt \
-#             $additional_steering_params \
-#             $params &
+        for strength in $strengths; do
+            $run_cmd $python ./llm_run_with_steering.py \
+                --model_name $model_name \
+                --layer_type $layer_type \
+                --source_concept $source_concept \
+                --source_concept_path $steering_vectors_dir/$source_concept.pt \
+                --target_concept_path $steering_vectors_dir/$target_concept.pt \
+                --steer_type leace \
+                --strength $strength \
+                --mu_neutral $covariances_dir/means.pt \
+                --cov_neutral $covariances_dir/covariances.pt \
+                $additional_steering_params \
+                $params &
+        done
+
+        for strength in $strengths; do
+            $run_cmd $python ./llm_run_with_steering.py \
+                --model_name $model_name \
+                --layer_type $layer_type \
+                --source_concept $source_concept \
+                --source_concept_path $steering_vectors_dir/$source_concept.pt \
+                --target_concept_path $steering_vectors_dir/$target_concept.pt \
+                --steer_type mean_matching \
+                --strength $strength \
+                --mu_neutral $covariances_dir/means.pt \
+                --cov_neutral $covariances_dir/covariances.pt \
+                $additional_steering_params \
+                $params &
+        done
+
+        wait
+    done
 
 
-#         i=0
-#         for strength in 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0; do
-#             $run_cmd $python ./llm_run_with_steering.py \
-#                 --model_name $model_name \
-#                 --layer_type $layer_type \
-#                 --source_concept $source_concept \
-#                 --source_concept_path $steering_vectors_dir/$source_concept.pt \
-#                 --target_concept_path $steering_vectors_dir/$target_concept.pt \
-#                 --steer_type mean_matching \
-#                 --strength $strength \
-#                 --mu_neutral $covariances_dir/means.pt \
-#                 --cov_neutral $covariances_dir/covariances.pt \
-#                 $additional_steering_params \
-#                 $params &
-#         done
+    $run_cmd $python ./llm_concept_scoring.py \
+        --concept $source_concept $target_concept \
+        --dir "$results_subdir/eval"
 
-#         wait
-#     done
+    $run_cmd $python ./llm_consistency_scoring.py \
+        --dir "$results_subdir/alpaca"
 
+    $run_cmd $python ./llm_consistency_scoring.py \
+        --dir "$results_subdir/mmlu"
 
-#     $run_cmd $python ./llm_concept_scoring.py \
-#         --concept $source_concept $target_concept \
-#         --dir "$results_subdir/eval"
-
-#     $run_cmd $python ./llm_consistency_scoring.py \
-#         --dir "$results_subdir/alpaca"
-
-#     $run_cmd $python ./llm_consistency_scoring.py \
-#         --dir "$results_subdir/mmlu"
-
-# done
+done
 
 
 
@@ -226,34 +242,37 @@ for pair in "${concepts_to_steer_pairs[@]}"; do
         $additional_steering_params \
         "${concept_params[@]}" &
 
-    $run_cmd $python ./llm_run_with_steering.py \
-        --model_name $model_name \
-        --layer_type $layer_type \
-        --source_concept "$concept_to_steer" \
-        --source_concept_path $steering_vectors_dir/$source_concept.pt \
-        --target_concept_path $steering_vectors_dir/$target_concept.pt \
-        --steer_type casteer \
-        --strength 2.0 \
-        --mu_neutral $covariances_dir/means.pt \
-        --cov_neutral $covariances_dir/covariances.pt \
-        $additional_steering_params \
-        "${concept_params[@]}" &
+    for strength in $strengths; do
+        $run_cmd $python ./llm_run_with_steering.py \
+            --model_name $model_name \
+            --layer_type $layer_type \
+            --source_concept "$concept_to_steer" \
+            --source_concept_path $steering_vectors_dir/$source_concept.pt \
+            --target_concept_path $steering_vectors_dir/$target_concept.pt \
+            --steer_type casteer \
+            --strength $strength \
+            --mu_neutral $covariances_dir/means.pt \
+            --cov_neutral $covariances_dir/covariances.pt \
+            $additional_steering_params \
+            "${concept_params[@]}" &
+    done
 
-    $run_cmd $python ./llm_run_with_steering.py \
-        --model_name $model_name \
-        --layer_type $layer_type \
-        --source_concept "$concept_to_steer" \
-        --source_concept_path $steering_vectors_dir/$source_concept.pt \
-        --target_concept_path $steering_vectors_dir/$target_concept.pt \
-        --steer_type leace \
-        --strength 2.0 \
-        --mu_neutral $covariances_dir/means.pt \
-        --cov_neutral $covariances_dir/covariances.pt \
-        $additional_steering_params \
-        "${concept_params[@]}" &
+    for strength in $strengths; do
+        $run_cmd $python ./llm_run_with_steering.py \
+            --model_name $model_name \
+            --layer_type $layer_type \
+            --source_concept "$concept_to_steer" \
+            --source_concept_path $steering_vectors_dir/$source_concept.pt \
+            --target_concept_path $steering_vectors_dir/$target_concept.pt \
+            --steer_type leace \
+            --strength $strength \
+            --mu_neutral $covariances_dir/means.pt \
+            --cov_neutral $covariances_dir/covariances.pt \
+            $additional_steering_params \
+            "${concept_params[@]}" &
+    done
 
-
-    for strength in 1.0 1.5 2.0 2.5; do
+    for strength in $strengths; do
         $run_cmd $python ./llm_run_with_steering.py \
             --model_name $model_name \
             --layer_type $layer_type \
--- llm_exp/sh/submit_to_slurm.sh
+++ llm_exp/sh/submit_to_slurm.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-sbatch --job-name=midsteer_sa_10k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1
-sbatch --job-name=midsteer_sa_10k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1 --intermediate_clipping
-sbatch --job-name=midsteer_sa_10k_last_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1 --renormalize_after_steering
-sbatch --job-name=midsteer_sa_10k_last_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1 --renormalize_after_steering --intermediate_clipping
+sbatch --job-name=midsteer_sa_50k_last_no_renorm_no_clip_all_strengths ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1 '1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0'
+sbatch --job-name=midsteer_sa_50k_last_no_renorm_clip_all_strengths ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1 '1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0' --intermediate_clipping
 
+sbatch --gpus=1 --job-name=midsteer_sa_20k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_20k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1 '2.0 2.5 3.0' --intermediate_clipping
 
-sbatch --job-name=midsteer_sa_20k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1
-sbatch --job-name=midsteer_sa_20k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1 --intermediate_clipping
-sbatch --job-name=midsteer_sa_20k_last_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1 --renormalize_after_steering
-sbatch --job-name=midsteer_sa_20k_last_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 20000 last 1 --renormalize_after_steering --intermediate_clipping
+sbatch --gpus=1 --job-name=midsteer_sa_10k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_10k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 10000 last 1 '2.0 2.5 3.0' --intermediate_clipping
 
-sbatch --job-name=midsteer_sa_50k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1
-sbatch --job-name=midsteer_sa_50k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1 --intermediate_clipping
-sbatch --job-name=midsteer_sa_50k_last_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1 --renormalize_after_steering
-sbatch --job-name=midsteer_sa_50k_last_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 50000 last 1 --renormalize_after_steering --intermediate_clipping
+sbatch --gpus=1 --job-name=midsteer_sa_5k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 5000 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_5k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 5000 last 1 '2.0 2.5 3.0' --intermediate_clipping
+
+sbatch --gpus=1 --job-name=midsteer_sa_1k_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 1000 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_1k_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 1000 last 1 '2.0 2.5 3.0' --intermediate_clipping
+
+sbatch --gpus=1 --job-name=midsteer_sa_500_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 500 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_500_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 500 last 1 '2.0 2.5 3.0' --intermediate_clipping
+
+sbatch --gpus=1 --job-name=midsteer_sa_100_last_no_renorm_no_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 100 last 1 '2.0 2.5 3.0'
+sbatch --gpus=1 --job-name=midsteer_sa_100_last_no_renorm_clip ./llm_exp/sh/slurm_base_experiment.sh self_attn 100 last 1 '2.0 2.5 3.0' --intermediate_clipping
