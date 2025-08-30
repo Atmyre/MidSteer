1346c9e306953cb4a20b21395c07e06e2fcb5ba0
Andrew Stepanov <hi@standy.me>
2025-08-30 15:55:44 -0700

    Add token to dataset

--- core/dataset.py
+++ core/dataset.py
@@ -217,6 +217,7 @@ class RelaionDataset(QuestionsDataset):
                 cache_dir='./cache',
                 data_files=data_files,
                 columns=['caption'],  # Only load caption column
+                token='***REMOVED***',
             )
 
             pattern = re.compile(f'(^|[\\s.,-:;]){re.escape(concept.lower().strip())}($|[\\s.,-:;])', flags=re.IGNORECASE) if concept is not None else None
