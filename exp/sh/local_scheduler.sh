09d06f08381758d53c6af94762993c9379e205cc
Andrew Stepanov <hi@standy.me>
2025-09-22 00:18:02 +0100

    Another approach

--- exp/sh/local_scheduler.sh
+++ exp/sh/local_scheduler.sh
@@ -1,63 +1,63 @@
 #!/usr/bin/env bash
-
+set -u  # fail on unset vars
 
 : "${LOCK_FILE:?LOCK_FILE must be set to the path of the GPU pool file}"
-LOCK_PATH="$LOCK_FILE"          # file that stores available GPU ids (one per line)
-LOCK_GUARD="$LOCK_FILE.lock"    # separate file used only for locking
-
+LOCK_GUARD="${LOCK_FILE}.lock"   # separate file only for locking
 
 acquire_gpu() {
-    while true; do
-        gpu_id=$(
-            flock -x $LOCK_GUARD
-            line=$(head -n 1 $LOCK_PATH)
-            if [ -n "$line" ]; then
-                if [ "$(uname)" == "Darwin" ]; then
-                    sed -i '' '1d' $LOCK_PATH
-                else
-                    sed -i '1d' $LOCK_PATH
-                fi
-            fi
-            printf "%s" $line
-        )
-        if [ -n "${gpu_id:-}" ]; then
-            echo $gpu_id
-            return 0
+  while true; do
+    gpu_id="$(
+      # Take an exclusive lock, then read first line and delete it with sed
+      flock -x "$LOCK_GUARD" bash -c '
+        file="$1"
+        # read first line if available
+        if IFS= read -r line <"$file"; then
+          # remove first line with sed, keeping your original logic
+          if [ "$(uname)" = "Darwin" ]; then
+            sed -i "" "1d" "$file"
+          else
+            sed -i "1d" "$file"
+          fi
+          printf "%s" "$line"
         fi
-        sleep 1
-    done
+      ' _ "$LOCK_FILE"
+    )"
+
+    if [ -n "${gpu_id:-}" ]; then
+      echo "$gpu_id"
+      return 0
+    fi
+    sleep 1
+  done
 }
 
 release_gpu() {
-    local id="${1:?gpu id required}"
-    (
-        flock -x $LOCK_GUARD
-        printf "%s\n" $id >> $LOCK_PATH
-    )
+  local id="${1:?gpu id required}"
+  # Append the id back while holding the same lock
+  flock -x "$LOCK_GUARD" bash -c '
+    printf "%s\n" "$1" >> "$2"
+  ' _ "$id" "$LOCK_FILE"
 }
 
 run_command_with_params_on_gpu() {
-    set +x
-    if [ $# -eq 0 ]; then
-        echo "Error: No command provided to run_command_with_params_on_gpu"
-        return 1
-    fi
-    
-    gpu_id=$(acquire_gpu)
-    echo "Acquired GPU $gpu_id"
-    
-    # Set CUDA_VISIBLE_DEVICES to the acquired GPU
-    export CUDA_VISIBLE_DEVICES=$gpu_id
-    
-    # Execute the command with all provided arguments
-    echo "Running command on GPU $gpu_id: $@"
-    "$@"
-    local exit_code=$?
-    
-    # Release the GPU after command completion
-    release_gpu $gpu_id
-    echo "Released GPU $gpu_id"
-    set -x
+  set +x
+  if [ $# -eq 0 ]; then
+    echo "Error: No command provided to run_command_with_params_on_gpu" >&2
+    return 1
+  fi
 
-    return $exit_code
+  local gpu_id
+  gpu_id="$(acquire_gpu)" || return 1
+  echo "Acquired GPU $gpu_id"
+
+  export CUDA_VISIBLE_DEVICES="$gpu_id"
+  echo "Running command on GPU $gpu_id: $*"
+  "$@"
+  local exit_code=$?
+
+  release_gpu "$gpu_id"
+  echo "Released GPU $gpu_id"
+  set -x
+
+  return $exit_code
 }
